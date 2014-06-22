8773c76fa314779e169623e7964706509649fef4
alan_liu <s871104a@msn.com>
2014-06-23 02:09:59 +0800

    File name still not be finished

--- toy/WebSpider/SoftwareSpider.py
+++ toy/WebSpider/SoftwareSpider.py
@@ -14,12 +14,14 @@ def main():
         datefmt = '%m/%d/%y %H:%M:%S'
     )
     
-    sft_spider = SoftwareSpider('software.ini', 'bbb')
-    software = 'SystemExplorer_portable'
-    sft_spider.get_download_link(sft_spider.get_ini_reader.get(software, 'download_page'), 
-                                 sft_spider.get_ini_reader.get(software, 'download_link_ptn'))
-'''
-'''
+    sft_spider = SoftwareSpider('software.ini', r'C:\Users\alan\Desktop\test_dir')
+#     software = 'SystemExplorer_portable'
+#     sft_spider.get_download_link(software,
+#                                  sft_spider.get_ini_reader.get(software, 'download_page'), 
+#                                  sft_spider.get_ini_reader.get(software, 'download_link_ptn'))
+    sft_spider.run()
+    
+    
 class SoftwareSpider(object):
     """
     @note: This class is aim to auto get software by a defined software pattern
@@ -34,6 +36,10 @@ class SoftwareSpider(object):
         """
         self._sft_ini = _sft_ini
         self._sft_dir = _sft_dir
+        
+        if self._sft_dir is None:
+            self._sft_dir = self._get_desktop_path()
+        
 
     def run(self):
         """
@@ -47,12 +53,12 @@ class SoftwareSpider(object):
             # create thread to download the software to '_sft_dir'
             url = self.get_ini_reader.get(sft_name, "download_page")
             url_ptn = self.get_ini_reader.get(sft_name, "download_link_ptn")
-            download_link = self.get_download_link(url, url_ptn)
-            if self.get_ini_reader.get(sft_name, "download_link_ptn"):
-                pass
             
+            download_link = self.get_download_link(sft_name, url, url_ptn)
             _logger.debug("dwonload_link: %s" %download_link)
-    
+
+            self._download_file(download_link, sft_name)
+            
     @property
     def get_ini_reader(self):
         
@@ -63,7 +69,7 @@ class SoftwareSpider(object):
         
         return self._config
         
-    def get_download_link(self, url, link_ptn):
+    def get_download_link(self,sft, url, link_ptn):
         """
         """
         _logger.debug("url:%s\nsearch download pattern:%s" %(url, link_ptn))
@@ -75,13 +81,15 @@ class SoftwareSpider(object):
         re_format = re.compile(link_ptn)
         
         matchs = [link for link in links if re_format.match(link) is not None]
-
         _logger.debug("Match links:")
-        _logger.debug(matchs)
-        
+        _logger.debug(matchs)        
         assert len(matchs) == 1, "Unique link not found!!"
         
-        return matchs[0]
+        download_link = matchs[0]
+        if self.get_ini_reader.has_option(sft, "dwonload_link_host"):
+            download_link = self.get_ini_reader.get(sft, "dwonload_link_host") + download_link
+        
+        return download_link
     
     def _parse_download_link(self, url):
         
@@ -102,46 +110,42 @@ class SoftwareSpider(object):
                 
         return all_links
     
+    def _get_desktop_path(self):
+        """
+        HKEY_CURRENT_USER
+        """
+        import _winreg
+    
+        desktop_path = ""
+    
+        aReg = _winreg.ConnectRegistry(None, _winreg.HKEY_CURRENT_USER)
+        aKey = _winreg.OpenKey(aReg, r"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders")
+    
+        try:
+            val= _winreg.QueryValueEx(aKey, "Desktop")            
+            _logger.debug("reg key value:%s" %str(val))
+            desktop_path = str(val[0])
+            
+        except EnvironmentError:
+            print traceback.format_exc()
+            print sys.exc_info()[0]       
+    
+        return desktop_path
+
+    def _download_file(self, url, file_name):
+
+        file_path = os.path.join(self._sft_dir, file_name)
+        _logger.debug("file_path = %s" %file_path)
+        
+        download_file = urllib2.urlopen(url)
+        with open(file_path, 'wb') as output:
+            output.write(download_file.read())
+
     def compare_software_version(self, sft_name1, sft_name2):
         """
         @note: Implement this if needed
         """
         return NotImplementedError
-    
-def get_desktop_path():
-    """
-    HKEY_CURRENT_USER
-    """
-    import _winreg
-
-    desktop_path = ""
-
-    aReg = _winreg.ConnectRegistry(None, _winreg.HKEY_CURRENT_USER)
-    aKey = _winreg.OpenKey(aReg, r"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders")
-
-    try:
-        val= _winreg.QueryValueEx(aKey, "Desktop")            
-        _logger.debug("reg key value:%s" %str(val))
-        desktop_path = str(val[0])
-        
-    except EnvironmentError:
-        print traceback.format_exc()
-        print sys.exc_info()[0]       
-
-    return desktop_path
-
-def download_file():
-    
-    import urllib2
-    
-    download_file = urllib2.urlopen('http://download.tuxfamily.org/notepadplus/6.6.4/npp.6.6.4.Installer.exe')
-    
-    file_path = os.path.join(get_desktop_path(), "npp.6.6.4.Installer.exe")
-    _logger.debug("file_path = %s" %file_path)
-    with open(file_path, 'wb') as output:
-        output.write(download_file.read())
-
-
 
 if __name__ == "__main__":
     main()
